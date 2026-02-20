80d076810c2ca7cdb8f0563a89fe08323b726d90
Bruno Antunes <58961959+dev-brunoantunes@users.noreply.github.com>
2026-02-19 21:15:31 -0300

    update react and java

---
+++ backend/menufy/src/main/java/com/menufy/menufy/food/FoodRepository.java
@@ -0,0 +1,6 @@
+package com.menufy.menufy.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
---
+++ backend/menufy/src/test/java/com/menufy/menufy/MenufyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menufy.menufy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenufyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
